a912e5ffad9331734090f84a84eda613936feb51
squidgyberries <squidgyberriesgit@protonmail.com>
2022-07-14 15:46:24 +0800

    more colours

--- src/colors.h
+++ src/colors.h
@@ -1,12 +1,22 @@
 #ifndef GAMEOFLIFE_COLORS_H
 #define GAMEOFLIFE_COLORS_H
 
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
+#define ANSI_COLOR_BLACK          "\x1b[30m"
+#define ANSI_COLOR_RED            "\x1b[31m"
+#define ANSI_COLOR_GREEN          "\x1b[32m"
+#define ANSI_COLOR_YELLOW         "\x1b[33m"
+#define ANSI_COLOR_BLUE           "\x1b[34m"
+#define ANSI_COLOR_MAGENTA        "\x1b[35m"
+#define ANSI_COLOR_CYAN           "\x1b[36m"
+#define ANSI_COLOR_DARK_WHITE     "\x1b[37m"
+#define ANSI_COLOR_BRIGHT_BLACK   "\x1b[90m"
+#define ANSI_COLOR_BRIGHT_RED     "\x1b[91m"
+#define ANSI_COLOR_BRIGHT_GREEN   "\x1b[92m"
+#define ANSI_COLOR_BRIGHT_YELLOW  "\x1b[93m"
+#define ANSI_COLOR_BRIGHT_BLUE    "\x1b[94m"
+#define ANSI_COLOR_BRIGHT_MAGENTA "\x1b[95m"
+#define ANSI_COLOR_BRIGHT_CYAN    "\x1b[96m"
+#define ANSI_COLOR_WHITE          "\x1b[97m"
+#define ANSI_COLOR_RESET          "\x1b[0m"
 
 #endif // GAMEOFLIFE_COLORS_H
